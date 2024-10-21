7abc0046a136212147a4cb37b3e75a64b9743271
Trebias <unitsss11@gmail.com>
2024-10-21 23:16:08 +0700

    test organizaton

--- app/eco/tests.py
+++ app/eco/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.test import Client
+from .models import Storage, Organization
 
-# Create your tests here.
+
+wastes = ['bio', 'glass', 'plastic']
+
+
+class OrganizationTestCase(TestCase):
+    def setUp(self):
+        org1 = Organization(coord_x=5, coord_y=3.3, name="OO-1")
+        org1.generate("bio", 10.5)
+        org1.generate("glass", 13.7)
+        org1.save()
+
+    def test_org_create(self):
+        org1 = Organization.objects.get(name="OO-1")
+        self.assertEqual(org1.get_name(), "OO-1")
+        self.assertEqual(org1.get_coords(), (5, 3.3))
+        self.assertEqual(org1.get_waste(), {i: 0.0 for i in wastes})
+
+    def test_org_generate(self):
+        org1 = Organization.objects.get(name="OO-1")
+        self.assertEqual(org1.get_waste()['bio'], 10.5)
+
+    def test_org_send(self):
+        org1 = Organization.objects.get(name="OO-1")
+        org1.send_to_storage('bio', 8)
+        org1.send_to_storage("glass", 15)
+        self.assertEqual(org1.get_waste(), {"bio": 2.5, "glass": 13.7, "plastic": 0})
